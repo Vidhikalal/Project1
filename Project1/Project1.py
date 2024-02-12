2f278ec0688aaac35dcdf3577bb1fc5ef6b003df
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-11 19:55:58 -0500

    Created a random function to generate random number between 1-6 and initialised user input for making team from 2 to 4

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -1,1 +1,20 @@
+#project pig
+import random
+def roll():#Creting a function to generate random value 
+    min_value=1
+    max_value=6
+    roll=random.randit(min_value,max_value)
+    return roll
 
+while True:
+ players=input("Enter the number of player(1-4): ")
+ if players.isdigit():
+    players= int(players)
+    if 2<= players <=4:
+       break
+    else:
+        print("Must be between 2-4 players.")
+ else:
+       print("Invalid,try again.")
+
+print(players)
